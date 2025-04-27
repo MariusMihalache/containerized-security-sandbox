e354f38d0d29c254753be2db7273d6de77e93c78
Mihalache Ionut Marius <98221678+MariusMihalache@users.noreply.github.com>
2025-04-27 20:37:11 +0300

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Startăm serviciile necesare
+freshclam &  # Update la semnături antivirus
+clamd &      # Demon ClamAV
+
+# Pornim scriptul de monitorizare
+/opt/scripts/capture-activity.sh
+
+# Ținem containerul activ
+tail -f /dev/null
 
